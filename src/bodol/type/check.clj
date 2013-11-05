7e2927534c4241cdae94c4fe1904d3f2ee48a2bb
Bodil Stokke <bodil@bodil.org>
2013-11-05 14:36:43 +0000

    Synced BODOL code.

--- src/bodol/type/check.clj
+++ src/bodol/type/check.clj
@@ -1,28 +1,14 @@
 (ns bodol.type.check
+  (:refer-clojure :exclude [resolve])
   (:require [bodol.types :as t]
             [bodol.lambda :as l]
             [bodol.monad :as m]
             [bodol.parser :as parser]
+            [bodol.type.vars :as v :refer [function Num Str Bool Sym]]
             [clojure.string :as str])
   (:import [bodol.types LCons LBoolean LNumber LString LSymbol]
-           [bodol.lambda Lambda]))
-
-(defprotocol IState
-  (fresh-var [this])
-  (fresh-lvar [this t])
-  (generic? [this t])
-  (lookup-env [this name])
-  (bind-env [this name value])
-  (set-nongen [this t])
-  (bind-lvar [this t lvar])
-  (unscope [this other])
-  (prune [this t])
-  (unify [this t1 t2])
-  (-unified [this])
-  (-env [this])
-  (-nongen [this])
-  (-lvars [this])
-  (-next [this]))
+           [bodol.lambda Lambda]
+           [bodol.type.vars Variable Oper]))
 
 (defprotocol SyntaxNode
   (analyse [this]))
@@ -30,131 +16,122 @@
 (defprotocol TypeValue
   (to-string [this state]))
 
-(defrecord Variable [id]
-  TypeValue
-  (to-string [this state]
-    (let [pruned (prune state this)]
-      (if (= pruned this)
-        id
-        (to-string pruned state)))))
-
-(defrecord Oper [name args]
-  TypeValue
-  (to-string [_ state]
-    (cond
-     (zero? (count args)) name
-     (= 2 (count args)) (str "(" (to-string (first args) state) " " name
-                             " " (to-string (second args) state) ")")
-     :else (str name " " (str/join " " (map #(to-string % state) args))))))
 
 
+(declare occurs-in-type? prune)
 
-(def Num (Oper. "Num" []))
-(def Str (Oper. "Str" []))
-(def Bool (Oper. "Bool" []))
-(def Sym (Oper. "Sym" []))
-
-(defn function [& args]
-  (if (<= (count args) 2)
-    (Oper. "→" (vec args))
-    (Oper. "→" [(first args) (apply function (rest args))])))
-
-
-
-(declare occurs-in? occurs-in-type?)
-
-(deftype State [unified env nongen lvars next]
-  IState
-  (fresh-var [_]
-    (let [inced (-> next int inc char)]
-      [(Variable. (str next)) (State. unified env nongen lvars inced)]))
-
-  (fresh-lvar [this t]
-    (if-let [lvar (lvars t)]
-      [lvar this]
-      (let [[lvar state] (fresh-var this)]
-        [lvar (bind-lvar state t lvar)])))
-
-  (bind-lvar [this t lvar]
-    (State. unified env nongen (assoc lvars t lvar) next))
-
-  (generic? [this t]
-    (not (occurs-in? this nongen t)))
-
-  (lookup-env [_ name]
-    (env name))
-
-  (bind-env [_ name value]
-    (State. unified (assoc env name value) nongen lvars next))
-
-  (set-nongen [_ t]
-    (State. unified env (conj nongen t) lvars next))
-
-  (unscope [_ other]
-    (State. (-unified other) env nongen (-lvars other) (-next other)))
-
-  (prune [this t]
-    (cond
-     (and (instance? Variable t)
-          (generic? this t)
-          (lvars t))
-     (prune this (lvars t))
-
-     (and (instance? Variable t)
-          (contains? unified t))
-      (prune this (unified t))
-
-      :else t))
-
-  (unify [this t1 t2]
-    (let [t1 (prune this t1)
-          t2 (prune this t2)]
-      (cond
-
-       (and (instance? Variable t1) (not= t1 t2))
-       (if (occurs-in-type? this t1 t2)
-         (throw (Error. (str "recursive unification between " t1 " and " t2)))
-         (State. (assoc unified t1 t2) env nongen lvars next))
-
-       (and (instance? Oper t1) (instance? Variable t2))
-       (unify this t2 t1)
-
-       (and (instance? Oper t1) (instance? Oper t2))
-       (if (or (not= (:name t1) (:name t2))
-               (not= (count (:args t1)) (count (:args t2))))
-         (throw (Error. (str "Type mismatch: " (to-string t1 this)
-                             " ≠ " (to-string t2 this))))
-         (loop [state this a (:args t1) b (:args t2)]
-           (if (and (seq a) (seq b))
-             (recur (unify state (first a) (first b)) (rest a) (rest b))
-             state)))
-
-       :else (throw (Error. (str "Cannot unify args " t1 " and " t2))))))
-
-  (-unified [_] unified)
-  (-env [_] env)
-  (-nongen [_] nongen)
-  (-lvars [_] lvars)
-  (-next [_] next)
-
-  Object
-  (toString [_]
-    (let [env (dissoc env "pair" "true" "cond" "zero" "pred" "times")]
-      (str unified " :: " env " :: " nongen
-           " :: " lvars " :: " next))))
-
-(defn occurs-in? [^State state ^Variable t l]
+(defn occurs-in? [state ^Variable t l]
   (some #(occurs-in-type? state t %) l))
 
-(defn occurs-in-type? [^State state ^Variable v type2]
+(defn occurs-in-type? [state ^Variable v type2]
   (let [v (prune state v)]
     (cond
      (= v type2) true
      (instance? Oper type2) (occurs-in? state v (:args type2))
      :else false)))
 
+(defn generic? [state t]
+  (not (occurs-in? state (:nongen state) t)))
+
+(defn prune [state t]
+  (cond
+   (and (instance? Variable t)
+        (generic? state t)
+        ((:lvars state) t))
+   (prune state ((:lvars state) t))
+
+   (and (instance? Variable t)
+        (contains? (:unified state) t))
+   (prune state ((:unified state) t))
+
+   :else t))
+
+
+
+(extend-type Variable
+  TypeValue
+  (to-string [this state]
+    (let [pruned (prune state this)]
+      (if (= pruned this)
+        (:id this)
+        (to-string pruned state)))))
+
+(extend-type Oper
+  TypeValue
+  (to-string [this state]
+    (let [args (:args this)
+          name (:name this)]
+      (cond
+       (zero? (count args))
+       name
+
+       (= 2 (count args))
+       (str "(" (to-string (first args) state) " " name
+            " " (to-string (second args) state) ")")
+
+       :else (str (str/join " " (map #(to-string % state) args)) " " name)))))
+
+
+
+(defn fresh-var [state]
+  (assoc state :next (-> (:next state) int inc char)))
+
+(defn bind-lvar [state t lvar]
+  (assoc-in state [:lvars t] lvar))
+
+(defn fresh-lvar [state t]
+  (if-let [lvar ((:lvars state) t)]
+    [lvar state]
+    (let [[lvar state] (fresh-var state)]
+      [lvar (bind-lvar state t lvar)])))
+
+(defn fresh-var [state]
+  (let [next (:next state)
+        inced (-> next int inc char)]
+    [(Variable. (str next)) (assoc state :next inced)]))
+
+(defn lookup-env [state name]
+  (when-let [val ((:env state) name)]
+    (if-let [m (:signature (meta val))] m val)))
+
+(defn bind-env [state name value]
+  (assoc-in state [:env name] value))
+
+(defn set-nongen [state t]
+  (assoc state :nongen (conj (:nongen state) t)))
+
+(defn unscope [state other]
+  (assoc other
+    :env (:env state)
+    :nongen (:nongen state)))
+
+(defn unify [state t1 t2]
+  (let [t1 (prune state t1)
+        t2 (prune state t2)]
+    (cond
+
+     (and (instance? Variable t1) (not= t1 t2))
+     (if (occurs-in-type? state t1 t2)
+       (throw (Error. (str "recursive unification between " t1 " and " t2)))
+       (assoc state :unified (assoc (:unified state) t1 t2)))
+
+       (and (instance? Oper t1) (instance? Variable t2))
+       (unify state t2 t1)
+
+       (and (instance? Oper t1) (instance? Oper t2))
+       (if (or (not= (:name t1) (:name t2))
+               (not= (count (:args t1)) (count (:args t2))))
+         (throw (Error. (str "Type mismatch: " (to-string t1 state)
+                             " ≠ " (to-string t2 state))))
+         (loop [state state a (:args t1) b (:args t2)]
+           (if (and (seq a) (seq b))
+             (recur (unify state (first a) (first b)) (rest a) (rest b))
+             state)))
+
+       :else (throw (Error. (str "Cannot unify args " t1 " and " t2))))))
+
 (defn fresh [t]
-  (fn [^State state]
+  (fn [state]
     (let [t (prune state t)]
       (cond
        (instance? Variable t)
@@ -176,7 +153,7 @@
     (if (= pruned t) t pruned)))
 
 (defn bind-arg [arg]
-  (fn [^State state]
+  (fn [state]
     (let [[argtype state] (fresh-var state)
           state (-> state
                     (bind-env (t/-value arg) argtype)
@@ -191,34 +168,34 @@
 (extend-protocol SyntaxNode
   LNumber
   (analyse [this]
-    (fn [^State state]
+    (fn [state]
       [Num state]))
 
   LString
   (analyse [this]
-    (fn [^State state]
+    (fn [state]
       [Str state]))
 
   LBoolean
   (analyse [this]
-    (fn [^State state]
+    (fn [state]
       [Bool state]))
 
   LSymbol
   (analyse [this]
-    (fn [^State state]
+    (fn [state]
       (let [name (t/-value this)
             sym (lookup-env state name)]
         (if sym
           ((fresh sym) state)
-          (throw (Error. (str "Undefined symbol " name " ::: " state)))))))
+          (throw (Error. (str "Undefined symbol " name)))))))
 
   LCons
   (analyse [this]
     (let [func (t/car this)
           args (t/cdr this)]
 
-      (fn [^State state]
+      (fn [state]
         (let [[functype state] ((analyse func) state)]
           (loop [args args
                  state state
@@ -232,7 +209,7 @@
 
   Lambda
   (analyse [this]
-    (fn [^State state]
+    (fn [state]
       (let [{:keys [args body]} (first (l/-clauses this))
             [argtypes new-state] (bind-args state args)
             [resulttype new-state] ((analyse body) new-state)]
@@ -241,26 +218,16 @@
 
 
 
-(defn fresh-state []
-  (let [state (State. {} {} #{} {} \a)
-        [var1 state] (fresh-var state)
-        [var2 state] (fresh-var state)
-        [var3 state] (fresh-var state)
-        pairtype (Oper. "×" [var1 var2])]
-    (-> state
-        (bind-env "+" (function Num Num Num)))))
+(defn fresh-state [scope]
+  {:unified {} :env scope :nongen #{} :lvars {} :next \a})
 
 (defn- check-sexp-m [sexp]
-  (fn [^State state]
+  (fn [state]
     ((analyse sexp) state)))
 
 (defn type-check
   ([prog state]
-     (m/map-state state (map check-sexp-m prog)))
-  ([prog] (type-check prog (fresh-state))))
-
-(defn result-str [[types state]]
-  (map #(to-string % state) types))
+     (m/map-state state (map check-sexp-m prog))))
 
 (defn print-result [[types state]]
   (doseq [type types]
@@ -268,6 +235,23 @@
 
 
 
-#_(-> (parser/parse "(ƒ double-add m n → (+ (+ m m) (+ n n)))")
-      type-check
+
+#_(-> (parser/parse "1337")
+      (type-check (fresh-state (bodol.scope/scope)))
+      print-result)
+
+#_(-> (parser/parse "rainbow-dash")
+      (type-check (fresh-state (bodol.scope/scope)))
+      print-result)
+
+#_(-> (parser/parse "(ƒ add a b → (+ a b))")
+      (type-check (fresh-state (bodol.scope/scope)))
+      print-result)
+
+#_(-> (parser/parse "(ƒ double-mult a b → (* (+ a a) (+ b b)))")
+      (type-check (fresh-state (bodol.scope/scope)))
+      print-result)
+
+#_(-> (parser/parse "(ƒ eq a a → #t a b → #f)")
+      (type-check (fresh-state (bodol.scope/scope)))
       print-result)
